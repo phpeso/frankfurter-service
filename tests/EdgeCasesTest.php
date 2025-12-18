6291c1c3f0c3c8d9c85eb3ca8cde27867f2f4a0e
Anton Smirnov <sandfox+git@sandfox.me>
2025-12-18 08:38:58 +0200

    More edge cases

--- tests/EdgeCasesTest.php
+++ tests/EdgeCasesTest.php
@@ -10,8 +10,12 @@ declare(strict_types=1);
 namespace Peso\Services\Tests;
 
 use ArrayObject;
+use GuzzleHttp\Psr7\Response;
+use Http\Mock\Client;
 use Peso\Core\Exceptions\RequestNotSupportedException;
+use Peso\Core\Requests\CurrentExchangeRateRequest;
 use Peso\Core\Responses\ErrorResponse;
+use Peso\Core\Services\SDK\Exceptions\HttpFailureException;
 use Peso\Services\FrankfurterService;
 use PHPUnit\Framework\TestCase;
 
@@ -28,5 +32,55 @@ final class EdgeCasesTest extends TestCase
             'Unsupported request type: "ArrayObject"',
             $response->exception->getMessage(),
         );
+    }
+
+    public function testHostname(): void
+    {
+        $http = new Client();
+        $http->setDefaultResponse(new Response(body: fopen(__DIR__ . '/data/rates/latest-EUR.json', 'r')));
+
+        $request = new CurrentExchangeRateRequest('EUR', 'USD');
+
+        // auto prefixes with https
+        $service = new FrankfurterService(hostname: 'api.self-hosted.local', httpClient: $http);
+        $service->send($request);
+        $httpRequest = $http->getLastRequest();
+        self::assertEquals('https://api.self-hosted.local/v1/latest?amount=1&base=EUR', (string)$httpRequest->getUri());
+
+        // keeps https
+        $service = new FrankfurterService(hostname: 'https://api.self-hosted.local', httpClient: $http);
+        $service->send($request);
+        $httpRequest = $http->getLastRequest();
+        self::assertEquals('https://api.self-hosted.local/v1/latest?amount=1&base=EUR', (string)$httpRequest->getUri());
+
+        // keeps http
+        $service = new FrankfurterService(hostname: 'http://api.self-hosted.local', httpClient: $http);
+        $service->send($request);
+        $httpRequest = $http->getLastRequest();
+        self::assertEquals('http://api.self-hosted.local/v1/latest?amount=1&base=EUR', (string)$httpRequest->getUri());
+    }
+
+    public function testInternalError(): void
+    {
+        $http = new Client();
+        $http->setDefaultResponse(new Response(500));
+
+        $service = new FrankfurterService(httpClient: $http);
+
+        $this->expectException(HttpFailureException::class);
+        $this->expectExceptionMessage('HTTP error 500. Response is ""');
+        $service->send(new CurrentExchangeRateRequest('EUR', 'USD'));
+    }
+
+    public function testNonJson404(): void
+    {
+        $http = new Client();
+        $http->setDefaultResponse(new Response(404, body: 'Not found'));
+
+        $service = new FrankfurterService(httpClient: $http);
+
+        $this->expectException(HttpFailureException::class);
+        $this->expectExceptionMessage('HTTP error 404. Response is "Not found"');
+        $service->send(new CurrentExchangeRateRequest('EUR', 'USD'));
     }
 }
